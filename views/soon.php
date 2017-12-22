3c3d02253f412fa0cff2c715d3b02eb61b568b3e
eshka <bugelmax@gmail.com>
2017-12-22 18:27:31 +0200

    create soon and under pages

---
+++ views/soon.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+ <meta charset="utf-8" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+ <title>Coming soon</title>
+ <?php include('./head.php'); ?>
+</head>
+<body>
+   <section class="soon_page home-logged-in">
+      <a href="#" class="logo">
+         <img src="../assets/images/wb_logo-nav.png" alt="">
+      </a>
+      <h2>
+         coming soon
+      </h2>
+   </section>
+   
+</body>
---
+++ views/under.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+ <meta charset="utf-8" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+ <title>Coming soon</title>
+ <?php include('./head.php'); ?>
+</head>
+<body>
+   <section class="under_page home-logged-in">
+      <a href="#" class="logo">
+         <img src="../assets/images/wb_logo-nav.png" alt="">
+      </a>
+      <h2>
+         under maintenance
+      </h2>
+   </section>
+   
+</body>
