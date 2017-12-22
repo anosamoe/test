650a7d1728a54517188e2ef02c1761fa739dc5c7
eshka <bugelmax@gmail.com>
2017-12-22 15:15:52 +0200

    create 404 page

---
+++ views/404.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+    <title>Home Logged Out</title>
+    <?php include('./head.php'); ?>
+</head>
+
+<body>
+	<section class="home-logged-in error-page">
+        <a href="#" class="logo">
+            <img src="../assets/images/wb_logo-nav.png" alt="">
+        </a>
+		<h2>Error 404</h2>
+		<a href="#" class="back-link">Back to the site</a>
+	</section>
+</body>
+
+</html>
---
+++ views/about.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+    <title>Home Logged Out</title>
+    <?php include('./head.php'); ?>
+</head>
+<html>
+<body>
+	<?php include('./header-logged-out.php'); ?>
+	<section class="home-banner">
+		
+	</section>
+	<div class="main-container home-logged-out">
+        <?php include('./footer.php'); ?>
+    </div>
+</body>
+</html>
