16855dc132753b49d7ab1af96195d3981710db9f
eshka <bugelmax@gmail.com>
2017-12-26 10:58:26 +0200

    create promotion_list page

---
+++ views/promotions_list.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+    <title>Promotions</title>
+    <?php include('./head_foot_menu/head.php'); ?>
+</head>
+
+<body>
+    <?php include('./head_foot_menu/header-logged-in.php'); ?>
+    <div class="main-container home-logged-in promotions-page">
+        <section class="games_filter">
+            <div class="filter_cont input-cont clear">
+                <h2>Promotions</h2>
+                <div class="games_filter_category select-arrow">
+                    <select>
+                        <option selected disabled>Welcome Bonuses</option>
+                        <option>option 1</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                    </select>
+                </div>
+            </div>
+            <div class="games_cont">
+                <div class="games_cont_item">
+                    <p class="title">Additional Bonuses</p>
+                    <div class="add_bonuses">
+                        <div class="add_bonuses_item">
+                            <a href="#" class="item_link_text">
+                                <img class="item_img" src="../assets/images/empty_prom_l.png" alt="" style="background-image: url(../assets/images/promotions/king.jpg)">
+                                <span>Friday Kings Of Spades</span>
+                            </a>
+                        </div>
+                        <div class="add_bonuses_item">
+                            <a href="#" class="item_link_text">
+                                <img class="item_img" src="../assets/images/empty_prom_l.png" alt="" style="background-image: url(../assets/images/promotions/pincher.jpg)">
+                                <span>Steam Your Streak</span>
+                            </a>
+                        </div>
+                        <div class="add_bonuses_item">
+                            <a href="#" class="item_link_text">
+                                <img class="item_img" src="../assets/images/empty_prom_l.png" alt="" style="background-image: url(../assets/images/promotions/baseball_field.jpg)">
+                                <span>Snap Your Skid</span>
+                            </a>
+                        </div>
+                        <div class="add_bonuses_item">
+                            <a href="#" class="item_link_text">
+                                <img class="item_img" src="../assets/images/empty_prom_l.png" alt="" style="background-image: url(../assets/images/promotions/magic_numbers.jpg)">
+                                <span>10 is the Magic Number</span>
+                            </a>
+                        </div>
+                        <div class="add_bonuses_item">
+                            <a href="#" class="item_link_text">
+                                <img class="item_img" src="../assets/images/empty_prom_l.png" alt="" style="background-image: url(../assets/images/promotions/boxing.jpg)">
+                                <span>MMA & Boxing Bad Beat</span>
+                            </a>
+                        </div>
+                        <div class="add_bonuses_item">
+                            <a href="#" class="item_link_text">
+                                <img class="item_img" src="../assets/images/empty_prom_l.png" alt="" style="background-image: url(../assets/images/promotions/spread_word.jpg)">
+                                <span>Spread the Word</span>
+                            </a>
+                        </div>
+                        <div class="add_bonuses_item">
+                            <a href="#" class="item_link_text">
+                                <img class="item_img" src="../assets/images/empty_prom_l.png" alt="" style="background-image: url(../assets/images/promotions/superman.jpg)">
+                                <span>Superman</span>
+                            </a>
+                        </div>
+                        <div class="add_bonuses_item">
+                            <a href="#" class="item_link_text">
+                                <img class="item_img" src="../assets/images/empty_prom_l.png" alt="" style="background-image: url(../assets/images/promotions/fresh_start.jpg)">
+                                <span>Fresh Start</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php include('./head_foot_menu/footer.php'); ?>
+    </div>
+
+</body>
+</html>
